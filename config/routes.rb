e9811f3a9a5751266cd6e035c351db68b4d6c76a
Hussain Rana <11besemrana@seecs.edu.pk>
2015-07-21 15:12:38 +0500

    Added layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+    # You can have the root of your site routed with "root"
+  root 'pages#home'
 
   get 'pages/contact'
 
   get 'pages/about'
+
+  get 'pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
