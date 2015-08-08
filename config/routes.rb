b3f6912e28837a91c214db1678a9e090dfa41226
Hussain Rana <11besemrana@seecs.edu.pk>
2015-08-08 08:51:31 +0500

    Added encrypted password

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :user #to get REST url's
-  # get 'users/new'
+  #get 'users/new'
+  # get '/signup'
 
     # You can have the root of your site routed with "root"
+   get 'users/:id' => 'users#show'
+   get '/signup' => 'users#show'
   root 'pages#home'
 
   get 'pages/contact'
@@ -18,7 +21,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+ 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
